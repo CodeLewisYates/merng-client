7d7337e19ac4555171292bdf2fd44453dd2a6d10
CodeLewisYates <lewis_yates@ymail.com>
2021-03-27 11:39:50 +0000

    init

---
+++ src/components/LikeButton.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+
+import { Button, Icon, Label } from "semantic-ui-react";
+import MyPopup from "../util/MyPopup";
+
+function LikeButton({ user, post: { id, likeCount, likes } }) {
+  const [liked, setLiked] = useState(false);
+  useEffect(() => {
+    if (user && likes.find((like) => like.username === user.username)) {
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [user, likes]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION, {
+    variables: { postId: id },
+  });
+
+  const likeButton = user ? (
+    liked ? (
+      <Button color="orange">
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      <Button color="orange" basic>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    <Button as={Link} to="/" color="orange" basic>
+      <Icon name="heart" />
+    </Button>
+  );
+
+  return (
+    <Button as="div" labelPosition="right" onClick={likePost}>
+      <MyPopup content={liked ? "Unlike" : "Like"}>{likeButton}</MyPopup>
+      <Label basic color="orange" pointing="left">
+        {likeCount}
+      </Label>
+    </Button>
+  );
+}
+
+const LIKE_POST_MUTATION = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+      likeCount
+    }
+  }
+`;
+
+export default LikeButton;
